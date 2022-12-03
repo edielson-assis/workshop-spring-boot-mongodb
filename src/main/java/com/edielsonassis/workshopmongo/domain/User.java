79378d6652c6760cfe6da24cac192cb7ac5eebbf
Edielson Assis <edielsonnice@hotmail.com>
2022-12-03 16:07:43 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/edielsonassis/workshopmongo/domain/User.java
+++ src/main/java/com/edielsonassis/workshopmongo/domain/User.java
@@ -8,14 +8,10 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 @Document
 public class User implements Serializable {
@@ -26,5 +22,11 @@ public class User implements Serializable {
     private String email;
 
     @DBRef(lazy = true)
-    final private List<Post> posts = new ArrayList<>();
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
