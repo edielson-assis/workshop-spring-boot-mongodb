58c5b1aa735488dd7286a8dd4711c0c1dd90bf37
Edielson Assis <edielsonnice@hotmail.com>
2022-12-03 20:58:30 -0300

    Consulta simples com query methods

--- src/main/java/com/edielsonassis/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/edielsonassis/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.edielsonassis.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Controller;
 
@@ -7,4 +9,6 @@ import com.edielsonassis.workshopmongo.domain.Post;
 
 @Controller
 public interface PostRepository extends MongoRepository<Post, String> {  
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
