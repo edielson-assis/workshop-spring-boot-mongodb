ae5511a7253e7c1d96da610faf7cf45200abdcaf
Edielson Assis <edielsonnice@hotmail.com>
2022-12-03 19:19:01 -0300

    Obtendo um post por id

---
+++ src/main/java/com/edielsonassis/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.edielsonassis.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edielsonassis.workshopmongo.domain.Post;
+import com.edielsonassis.workshopmongo.repositories.PostRepository;
+import com.edielsonassis.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+    
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
